c54471348b1f8734486da2bc5e8aa878ffa7fe2e
CameronD73 <56762299+CameronD73@users.noreply.github.com>
2025-03-25 13:28:09 +1000

    rewording comments

--- utils/sql-scripts/check-ICW-discrepancies.sql
+++ utils/sql-scripts/check-ICW-discrepancies.sql
@@ -1,9 +1,9 @@
 -- NOTE - at the moment these don't work well in combination of old recorded segments and new ICW (2024 scanning)
--- as we have added chromosome = -2 for times 23andMe hide all segment data.
+-- as we have added chromosome = -2 for those times where 23andMe hides all segment data.
 
 -- look at discrepancies between 3-way and 2-way ICW tables..
 -- if w3 chr = -1 then we have not yet checked the ID2-ID3 segments
--- id w3.chr = 0 then there should be precise 1:1 correspondence, but the left join shows problems where w2.chr is null
+-- id w3.chr = 0 then there should be precise 1:1 correspondence, but the left join is done to show problems (where w2.chr is null)
 
 select a1.name, a2.name, a3.name, w3.chromosome as chr3,w2.chromosome as chr2, w2.start*0.000001 as start, w2.end*0.000001 as end, IDprofile, ID2, ID3
 from ICWsets as w3 LEFT JOIN ICWSets2way as w2 USING( IDprofile, ID2, ID3)
